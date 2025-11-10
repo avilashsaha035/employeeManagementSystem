58e69c7389f32ffcb5ac722e50a8b5d3e34a8ef8
Avilash Saha <93313393+avilashsaha035@users.noreply.github.com>
2025-11-10 16:43:03 +0600

    add config.php file for db connection.

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+// ====== Local ======= //
+$servername = "localhost";  // from your MySQL settings
+$username = "root";        // your username
+$password = "";           // your db password
+$dbname = "ems";         // db name
+
+// ====== Live ======= //
+$servername = "sql111.infinityfree.com";  // from your MySQL settings
+$username = "if0_40340974";       	 // your username
+$password = "Avi24lash35";              // your db password
+$dbname = "if0_40340974_ems";          // db name
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
